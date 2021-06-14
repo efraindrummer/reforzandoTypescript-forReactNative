1a358b9f57713840fcbc5f965686bb7725967f3c
Efrain May <efrain_may_75@hotmail.com>
2021-06-14 13:23:42 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { TiposBasicos } from './typescript/TiposBasicos';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-2">
+      <h1>Introduccion a TS - React</h1>
+      <hr />
+      <TiposBasicos />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TiposBasicos = () => {
+    return (
+        <>
+            <h3>Tipos Basicos</h3>   
+        </>
+    )
+}

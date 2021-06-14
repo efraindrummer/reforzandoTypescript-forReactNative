5331221b750f7406bf69b4983b0505918ab121f1
Efrain May <efrain_may_75@hotmail.com>
2021-06-14 13:44:22 -0500

    objetos e interfaces

--- src/typescript/ObjetosLiterales.tsx
+++ src/typescript/ObjetosLiterales.tsx
@@ -1,11 +1,34 @@
 
+interface Persona {
+    nombreCompleto: string;
+    edad: number;
+    direccion: Direccion;
+}
+
+interface Direccion {
+    pais: string;
+    casaNo: number;
+}
+
 export const ObjetosLiterales = () => {
     
-    
+    const persona: Persona = {
+        nombreCompleto: 'Efrain',
+        edad: 22,
+        direccion: {
+            pais: 'Mexico',
+            casaNo: 123
+        }
+    }
 
     return (
         <>
-            <h3>Objetos Literales</h3>   
+            <h3>Objetos Literales</h3>
+            <code>
+                <pre>
+                    {JSON.stringify(persona, null, 2)}        
+                </pre>
+            </code> 
         </>
     )
 }

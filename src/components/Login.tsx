066a5b8c2e4c9bf638e6d43d77c78f442aa541a3
Efrain May <efrain_may_75@hotmail.com>
2021-06-15 00:23:03 -0500

    useReducer + AuthReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,61 @@
+import { useEffect, useReducer } from "react";
 
+interface AuthState {
+    validando: boolean;
+    token: string | null;
+    username: string;
+    nombre: string;
+}
+
+const initialState: AuthState = {
+    validando: true,
+    token: null,
+    username: '',
+    nombre: ''
+}
+
+type AuthAction = { type: 'logout' }
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState  => {
+
+    switch (action.type) {
+        case 'logout':
+            return {
+                validando: false,
+                token: null,
+                username: '',
+                nombre: ''
+            }
+              
+        default:
+            return state;
+    }
+}   
 
 export const Login = () => {
+
+    const [{validando}, dispatch] = useReducer(authReducer, initialState);
+
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch({ type: 'logout'});
+        }, 1500);
+    }, []);
+
+    if(validando){
+        return (
+            <>
+                <h3>Login</h3>
+                <div className="alert alert-info">
+                    Validando...
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <h3>Login</h3>
-
-            <div className="alert alert-info">
-                Validando...
-            </div>
-
             <div className="alert alert-danger">
                 No autenticado
             </div>
